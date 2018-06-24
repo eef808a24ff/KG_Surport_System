3e7bbb73878d74a0880a7618242cbd64680df74b
eef808a24ff <fansluck@qq.com>
2018-06-24 23:54:17 +0800

    完善了请求方式

--- app/src/main/java/com/fan/keproject/kg_surport_system/FindByName.java
+++ app/src/main/java/com/fan/keproject/kg_surport_system/FindByName.java
@@ -1,148 +1,62 @@
 package com.fan.keproject.kg_surport_system;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import android.content.Context;
+import android.provider.CalendarContract;
+import android.util.Log;
 
-//import org.apache.jena.query.Query;
-//import org.apache.jena.query.QueryExecution;
-//import org.apache.jena.query.QueryExecutionFactory;
-//import org.apache.jena.query.QueryFactory;
-//import org.apache.jena.query.QuerySolution;
-//import org.apache.jena.query.ResultSet;
-//import org.apache.jena.rdf.model.Literal;
-//import org.apache.jena.rdf.model.Resource;
-
+import com.hp.hpl.jena.graph.query.QueryHandler;
 import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryException;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.Syntax;
-import com.hp.hpl.jena.rdf.model.Literal;
-import com.hp.hpl.jena.rdf.model.RDFNode;
-
-
-
-
-import java.util.ArrayList;
-import java.util.HashSet;
 
 
 public class FindByName {
 
-    public String findMovie(String input)
-    {
+    static ResultSet resultSet;
+    QueryExecution exec;
+//    static boolean flag=true;
 
-        HashSet<String> directors = new HashSet<>();
-        HashSet<String> musicians = new HashSet<>();
-        HashSet<String> countries = new HashSet<>();
-        HashSet<String> actors = new HashSet<>();
-        HashSet<String> descriptions = new HashSet<>();
-        HashSet<String> taxonomies = new HashSet<>();
-
-        ArrayList<String> endResult = new ArrayList<>();
-
-        String queryString=
-                "PREFIX dbo: <http://dbpedia.org/ontology/>"+
-                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>"+
-                        "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"+
-                        "PREFIX dbp: <http://dbpedia.org/property/>"+
-                        "PREFIX dct: <http://purl.org/dc/terms/>"+
-                        "PREFIX foaf: <http://xmlns.com/foaf/0.1/>"+
-                        "SELECT  ?title ?Director "
-                        +"?Starring   ?Description"
-                        +"?Taxonomy "+
-                        "WHERE {"+
-                        "?Film rdf:type dbo:Film ."+
-                        "?Film foaf:name ?title ."+
-                        "optional{?Film dbo:director  ?Director}."+
-                        "optional{?Film dbo:starring  ?Starring.}"+
-                        "optional{?Film rdfs:comment ?Description.}"+
-                        "?Film dct:subject   ?Taxonomy FILTER( ?title =\""+input+"\"@en  && lang(?title) = 'en' && lang(?Description) = 'en').} ";
-
-
+    public ResultSet findMovie(String queryString) {
+//        flag=false;
+        Log.e("query", queryString);
         Query query = QueryFactory.create(queryString);
-        QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+        exec= QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", query);
+        ResultSet resultSet = exec.execSelect();
+//        closeExec();
+        return resultSet;
+    }
+//    public boolean isExecClosed(){
+//        return flag;
+//    }
 
-        ResultSet result = exec.execSelect();
+//    public void closeExec(){
+//        exec.close();
+//        flag=true;
+//        Log.e("exec","shutdown");
+//    }
 
-        try {
-            while(result.hasNext()){
-                QuerySolution soln = result.nextSolution();
-
-                try {
-                    if(soln.getResource("Director")!=null)directors.add(soln.getResource("Director").toString().replace("http://dbpedia.org/resource/", ""));
-
-                } catch (ClassCastException e) {
-                    if(soln.getLiteral("Director")!=null)directors.add(soln.getLiteral("Director").getLexicalForm());
-                }
-
-                try {
-                    if(soln.getLiteral("title")!=null)countries.add(soln.getLiteral("title").getLexicalForm());
-
-                } catch (ClassCastException e) {
-                }
-
-                try {
-                    if(soln.getResource("Starring")!=null)actors.add(soln.getResource("Starring").toString().replace("http://dbpedia.org/resource/", ""));
-
-                } catch (ClassCastException e) {
-                    if(soln.getLiteral("Starring")!=null)musicians.add(soln.getLiteral("Starring").getLexicalForm());
-                }
-                try {
-                    if(soln.getLiteral("Description")!=null)descriptions.add(soln.getLiteral("Description").getLexicalForm());
-
-                } catch (ClassCastException e) {
-                    if(soln.getResource("Description")!=null)countries.add(soln.getResource("Description").toString().replace("http://dbpedia.org/resource/", ""));
-                }
-                try {
-                    if(soln.getResource("Taxonomy")!=null)taxonomies.add(soln.getResource("Taxonomy").toString().replace("http://dbpedia.org/resource/Category:", ""));
-
-                } catch (ClassCastException e) {
-                    if(soln.getLiteral("Taxonomy")!=null)musicians.add(soln.getLiteral("Taxonomy").getLexicalForm());
-                }
+    public void run(final String queryString) {
+        new Thread() {
+            @Override
+            public void run() {
+                Log.e("Thread4","new");
+                resultSet = findMovie(queryString);
+                finish.finishRequest();
             }
-        } finally {
-            exec.close();
-        }
-        ArrayList<HashSet<String>> sets = new ArrayList<>();
-        sets.add(directors);
-        sets.add(actors);
-        sets.add(descriptions);
-        sets.add(taxonomies);
-
-        for (int i = 0; i < sets.size(); i++) {
-            if(sets.get(i).size()>0){
-                endResult.add(sets.get(i).toString().substring(1, sets.get(i).toString().length()-1).replace("_", " "));
-            }
-            else{
-                endResult.add("---");
-            }
-        }
-        String content="";
-        //System.out.println(endResult.size());
-        for(int j=0;j<endResult.size();j++)
-        {
-            content=content+endResult.get(j)+";;";
-        }
-
-//        System.out.println(content);
-
-        return content;
-
+        }.start();
+        Log.e("Thread3","dead");
     }
 
-    public void writeFile(String fileName,String content) throws IOException
-    {
-        if(fileName.contains("/"))
-        {
-            fileName=fileName.replaceAll("[/.():]", "");
-        }
-        PrintWriter pw = new PrintWriter( new FileWriter( "/Users/wangzehui/Desktop/Semantic Web Tech/DBData/"+fileName+".txt" ) );
-        pw.print( content );
-        pw.close( );
+    public static onFinishRequestListener finish;
+
+    interface onFinishRequestListener{
+        void finishRequest();
     }
 
+    public static void setOnFinishRequestListener(onFinishRequestListener finishIt){
+        finish=finishIt;
+    };
 }
